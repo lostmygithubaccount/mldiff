6e01d5f86008702420a01e2d238e6adff8287415
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2020-12-19 02:52:07 +0000

    commit from codespace

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup
 
 setup(
     name="mldiff",
-    version=open(Path(__file__).parent.absolute().joinpath("mldiff", "VERSION")).read().strip(),
+    version=open(Path(__file__).parent.absolute().joinpath("mldiff", "VERSION"))
+    .read()
+    .strip(),
     description="diff data with ml",
     url="https://github.com/lostmygithubaccount/mldiff",
     author="Cody",
